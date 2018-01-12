66915c749375522a5e2b8778a71f5e961a8f8e45
moltenshark <alex@berez.com>
2018-01-11 21:17:11 -0600

    fix

--- models/html-pdf.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('./resume.html', 'utf8');
-var options = { format: 'Letter' };
- 
-pdf.create(html, options).toFile('./resume.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});
