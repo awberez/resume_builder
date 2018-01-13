5fb9696b7c487d4df200e4509267494063e466f4
moltenshark <alex@berez.com>
2018-01-12 20:17:23 -0600

    working build page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -44,18 +44,6 @@ app.get("/skills/:id", function(req, res) {
     });
 });
 
-app.get("/buildresume/:id", function(req, res) {
-  // Handlebars 
-  //res.render("build");
-  db.User.findOne({
-      where: {
-        id: req.params.id,
-      }
-    }).then(function(dbUser) {
-    res.render("build", { user: dbUser });
-    });
-});
-
 app.get("/education/:id", function(req, res) {
   // Handlebars 
   //res.render("build");
@@ -68,4 +56,87 @@ app.get("/education/:id", function(req, res) {
     });
 });
 
+/*app.get("/buildresume/:id", function(req, res) {
+  // Handlebars 
+  //res.render("build");
+  db.User.findOne({
+      where: {
+        id: req.params.id,
+      }
+    }).then(function(dbUser) {
+    res.render("build", { user: dbUser });
+    });
+});*/
+
+app.get("/buildresume/:id", function(req, res) {
+  db.User.findOne({
+    where: {
+      id: req.params.id,
+    }
+  }).then(function(dbUser) {
+    db.Experience.findAll({
+      attributes: [
+        "tagOne", "tagTwo", "tagThree"
+      ],
+      where: {
+        UserId: req.params.id
+      },
+    }).then(function(dbExperience) {
+      db.Education.findAll({
+        attributes: [
+          "tagOne", "tagTwo", "tagThree"
+        ],
+        where: {
+          UserId: req.params.id
+        },
+      }).then(function(dbEducation) {
+        db.Work.findAll({
+          attributes: [
+            "tagOne", "tagTwo", "tagThree"
+          ],
+          where: {
+            UserId: req.params.id
+          },
+        }).then(function(dbWork) {
+          var data = {
+            expTags: dbExperience,
+            eduTags: dbEducation,
+            workTags: dbWork
+                  }
+          var dataTags = {};
+          for (let object of data.expTags) {
+            if (object.tagOne != "") dataTags[object.tagOne] = object.tagOne;
+            if (object.tagTwo != "") dataTags[object.tagTwo] = object.tagTwo;
+            if (object.tagThree != "") dataTags[object.tagThree] = object.tagThree;
+          }
+          for (let object of data.eduTags) {
+            if (object.tagOne != "") dataTags[object.tagOne] = object.tagOne;
+            if (object.tagTwo != "") dataTags[object.tagTwo] = object.tagTwo;
+            if (object.tagThree != "") dataTags[object.tagThree] = object.tagThree;
+          }
+          for (let object of data.workTags) {
+            if (object.tagOne != "") dataTags[object.tagOne] = object.tagOne;
+            if (object.tagTwo != "") dataTags[object.tagTwo] = object.tagTwo;
+            if (object.tagThree != "") dataTags[object.tagThree] = object.tagThree;
+          }
+          var tagArr = [];
+          for (let tag in dataTags) tagArr.push({ "tag": tag } );
+          res.render("build", { user: dbUser, tags: tagArr });
+        });
+      });
+    });
+  });
+});
+
 };
+
+
+
+
+
+
+
+
+
+
+
